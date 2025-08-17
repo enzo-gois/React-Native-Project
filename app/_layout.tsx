cf25502a3e32985b4b7030f968b6bf27b99642c0
enzo gois <enzoab26@outlook.com>
2025-08-17 17:32:40 -0300

    auth login with appwrite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,56 +1,35 @@
-import { Stack, useRouter } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState } from "react";
+import { AuthProvider, useAuth } from "@/lib/auth-context";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
 
-SplashScreen.preventAutoHideAsync();
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { user, isLoadingUser } = useAuth();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const inAuthGroup = segments[0] === "auth";
+
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/");
+    }
+  }, [user, segments]);
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
-  const router = useRouter();
-
-  const [isReady, setReady] = useState(false);
-
-  const [isAuth, setAuth] = useState(false);
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        setAuth(false);
-      } catch (e) {
-      } finally {
-        setReady(true);
-      }
-    };
-
-    checkAuth();
-  }, []); 
-
-  useEffect(() => {
-    if (!isReady) {
-      return; 
-    }
-
-    if (!isAuth) {
-      router.replace("/auth");
-    }
-    else {
-      router.replace("/(tabs)");
-    }
-  }, [isReady, isAuth]); 
-
-  useEffect(() => {
-    if (isReady) {
-      SplashScreen.hideAsync();
-    }
-  }, [isReady]);
-
-  if (!isReady) {
-    return null;
-  }
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="auth" options={{ headerShown: false }} />
-    </Stack>
+    <AuthProvider>
+      <RouteGuard>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="auth" options={{ headerShown: false }} />
+        </Stack>
+      </RouteGuard>
+    </AuthProvider>
   );
 }
